4f78e0820d9d70aaf5b09eb80bb56fbf5bb39616
Can Küçükaslan <dgncn.kcksln@hotmail.com>
2019-12-01 17:06:36 +0300

    add comment

--- InTheVision/Models/SettingsMv.cs
+++ InTheVision/Models/SettingsMv.cs
@@ -11,6 +11,8 @@ namespace InTheVision.Models
 
         public int getMovieCount()
         {
+            // Tüm filmlerin sayısını döndürür.
+            
             int count = _db.Movies.Count();
             return count;
         }
